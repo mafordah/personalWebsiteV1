508a292d71b7172bd79b89f14858744404179d70
Miya Fordah <42814756+mafordah@users.noreply.github.com>
2022-04-27 17:51:03 -0600

    backup
    backup for local restore

--- js/script.js
+++ js/script.js
@@ -6,23 +6,23 @@ import { LineGeometry } from './three/examples/jsm/lines/LineGeometry.js';
 
 
 
-let line, lineShadow, renderer, scene, camera, controls;
+let line, lineShadow, renderer, scene, camera, composer;
 let matLine, shadowLine;
 
 const points = [];
-const pointNumber = 20;
+const pointNumber = 15;
 const geometry = new LineGeometry();
 const shadowGeometry = new LineGeometry();
-const width = 6;
+const width = 4.5;
 var setRotate = true;
 var entered = false;
 
 //Div for spline click boundary
 var bounds = document.getElementById("bounds");
-bounds.style.width = (window.innerHeight * 0.4) + "px";
+var boundHeight = 0.13
+bounds.style.width = (window.innerHeight * boundHeight).toString() + "px"; //intial height
 
-//Div for entry text
-var entryText = document.getElementById("entry");
+// var entryText = document.getElementById("entry");
 
 var rotateSpeed = 0.01;
 
@@ -30,7 +30,8 @@ var rotateSpeed = 0.01;
 let insetWidth;
 let insetHeight;
 
-var spline;
+var spline = new THREE.CatmullRomCurve3(points, false, "catmullrom", 1.0);
+
 
 init();
 animate();
@@ -57,18 +58,18 @@ function init() {
 
     matLine = new LineMaterial({
         color: 0x404040,
-        linewidth: 2,
+        linewidth: 0.18,
         // in world units with size attenuation, pixels otherwise
-        //worldUnits: true,
+        worldUnits: true,
         dashed: false,
         alphaToCoverage: true,
     });
 
     shadowLine = new LineMaterial({
         color: 0xd1d1d1,
-        linewidth: 3,
+        linewidth: 0.25,
         // in world units with size attenuation, pixels otherwise
-        //worldUnits: true,
+        worldUnits: true,
         dashed: false,
         alphaToCoverage: true,
     });
@@ -77,7 +78,7 @@ function init() {
 
     line = new Line2(geometry, matLine);
     lineShadow = new Line2(shadowGeometry, shadowLine);
-    lineShadow.position.set(0, -15, 0);
+    lineShadow.position.set(0, -14, 0);
     lineShadow.scale.set(1.2, 1.2, 1.2);
 
     line.geometry.center();
@@ -94,7 +95,6 @@ function init() {
 function getPosition() {
     const positions = [];
     const shadowPositions = [];
-    spline = new THREE.CatmullRomCurve3(points, false, "catmullrom", 1.5);
     const divisions = Math.round(12 * points.length);
     const point = new THREE.Vector3();
 
@@ -131,15 +131,12 @@ function getNewPosition() {
 
     if (setRotate) {
         for (let i = 0, j = pointNumber; i < j; i++) {
-            //console.log(i, points[i]);
             gsap.to(points[i], {
                 duration: durationSec,
                 x: gsap.utils.random(-width, width),
                 y: gsap.utils.random(-width, width),
                 z: gsap.utils.random(-width, width),
                 ease: "rough({ template: none.out, strength: 1, points: 10, taper: none, randomize: true, clamp: true})",
-                //repeat:-1,
-                //repeatRefresh: true
             });
         }
         setTimeout(getNewPosition, durationMil);
@@ -147,38 +144,31 @@ function getNewPosition() {
 }
 
 
-function makeSignature() {
+function drawPage() {
     const duration = 1;
     const cameraDuration = 2.3;
+
+    //Det. points for unwrap
     // var signature = [
-    //     { x: -120, y: 0, z: 0 },
-    //     { x: -108, y: 0, z: 0 },
-    //     { x: -102, y: 0, z: 0 },
-    //     { x: -96, y: 0, z: 0 },
-    //     { x: -90, y: 0, z: 0 },
-    //     { x: -84, y: 0, z: 0 },
-    //     { x: -78, y: 0, z: 0 },
-    //     { x: -72, y: 0, z: 0 },
-    //     { x: -66, y: 0, z: 0 },
-    //     { x: -60, y: 0, z: 0 },
-    //     { x: -54, y: 0, z: 0 },
     //     { x: -48, y: 0, z: 0 },
-    //     { x: -42, y: 0, z: 0 },
-    //     { x: -20, y: 0, z: 0 },
-    //     { x: -5, y: 0, z: 0 },
-    //     { x: -4, y: 0, z: 0 },
-    //     { x: -3, y: 0, z: 0 },
-    //     { x: -2, y: 0, z: 0 },
-    //     { x: -1, y: 0, z: 0 },
+    //     { x: -36, y: 0, z: 0 },
+    //     { x: -30, y: 0, z: 0 },
+    //     { x: -24, y: 0, z: 0 },
+    //     { x: -18, y: 0, z: 0 },
+    //     { x: -12, y: 0, z: 0 },
+    //     { x: -6, y: 0, z: 0 },
     //     { x: -0, y: 0, z: 0 },
-    //     { x: -0, y: 0, z: 0 },
+    //     { x: 6, y: 0, z: 0 },
+    //     { x: 12, y: 0, z: 0 },
+    //     { x: 18, y: 0, z: 0 },
+    //     { x: 24, y: 0, z: 0 },
+    //     { x: 36, y: 0, z: 0 },
+    //     { x: 48, y: 0, z: 0 },
+    //     { x: 54, y: 0, z: 0 },
     // ];
 
     var signature = [
-        { x: -60, y: 0, z: 0 },
-        { x: -54, y: 0, z: 0 },
         { x: -48, y: 0, z: 0 },
-        { x: -42, y: 0, z: 0 },
         { x: -36, y: 0, z: 0 },
         { x: -30, y: 0, z: 0 },
         { x: -24, y: 0, z: 0 },
@@ -190,71 +180,25 @@ function makeSignature() {
         { x: 12, y: 0, z: 0 },
         { x: 18, y: 0, z: 0 },
         { x: 24, y: 0, z: 0 },
-        { x: 30, y: 0, z: 0 },
         { x: 36, y: 0, z: 0 },
-        { x: 42, y: 0, z: 0 },
         { x: 48, y: 0, z: 0 },
         { x: 54, y: 0, z: 0 },
     ];
 
-    // var signature = [
-    //     { x: 0, y: 60, z: 0 },
-    //     { x: 0, y: 57, z: 0 },
-    //     { x: 0, y: 54, z: 0 },
-    //     { x: 0, y: 51, z: 0 },
-    //     { x: 0, y: 48, z: 0 },
-    //     { x: 0, y: 45, z: 0 },
-    //     { x: 0, y: 42, z: 0 },
-    //     { x: 0, y: 39, z: 0 },
-    //     { x: 0, y: 36, z: 0 },
-    //     { x: 0, y: 33, z: 0 },
-    //     { x: 0, y: 30, z: 0 },
-    //     { x: 0, y: 27, z: 0 },
-    //     { x: 0, y: 24, z: 0 },
-    //     { x: 0, y: 18, z: 0 },
-    //     { x: 0, y: 15, z: 0 },
-    //     { x: 0, y: 4, z: 0 },
-    //     { x: 0, y: 3, z: 0 },
-    //     { x: 0, y: 2, z: 0 },
-    //     { x: 0, y: 0, z: 0 },
-    //     { x: 0, y: 0, z: 0 },
-    //     { x: 0, y: 0, z: 0 },
-    // ];
-
-    // var signature = [
-    //     { x: 0, y: -45, z: 0 },
-    //     { x: 0, y: -42, z: 0 },
-    //     { x: 0, y: -39, z: 0 },
-    //     { x: 0, y: -36, z: 0 },
-    //     { x: 0, y: -33, z: 0 },
-    //     { x: 0, y: -30, z: 0 },
-    //     { x: 0, y: -27, z: 0 },
-    //     { x: 0, y: -24, z: 0 },
-    //     { x: 0, y: -18, z: 0 },
-    //     { x: 0, y: -15, z: 0 },
-    //     { x: 0, y: -12, z: 0 },
-    //     { x: 0, y: -9, z: 0 },
-    //     { x: 0, y: -6, z: 0 },
-    //     { x: 0, y: -3, z: 0 },
-    //     { x: 0, y: 0, z: 0 },
-    //     { x: 0, y: 1, z: 0 },
-    //     { x: 0, y: 2, z: 0 },
-    //     { x: 0, y: 3, z: 0 },
-    //     { x: 0, y: 4, z: 0 },
-    //     { x: 0, y: 5, z: 0 },
-    // ];
 
     //Stop previous animation
     for (let i = 0, j = pointNumber; i < j; i++) {
         gsap.killTweensOf(points[i]);
     }
 
+    //Readjust tension for unwrap
+    gsap.to(spline, { tension: 1.0 });
+
+    //Start new timeline
     var move = gsap.timeline({});
 
-    //Move Camera
-    gsap.to(camera.position, { duration: cameraDuration, y: 15, ease: "power2.inOut" });
-    //gsap.to(camera.position, { duration: cameraDuration, y: 30 });
-
+    //Move camera positioning
+    // gsap.to(camera.position, { duration: cameraDuration, y: 13, ease: "power2.inOut" });
 
     //Decide rotation duration based on current rotation
     if (line.rotation.y > Math.PI) {
@@ -276,20 +220,21 @@ function makeSignature() {
         }, "<2%");
     }
 
-    //Cleanup: Remove entry text 
-    move.add(function(){entryText.style.display = "none"});
+
+    /*CLEANUP*/
+    //Remove hero bounds
+    // move.add(function () { bounds.style.display = "none" })
+    //Move scene to center
+    gsap.to(scene.position, { x: 0, y: 0 });
 
 }
-
-// function getRandom(min, max) {
-//     return gsap.utils.random(min, max);
-// }
 
 
 function onWindowResize() {
     //Reset bounds of spline
-    bounds.style.width = (window.innerHeight * 0.4) + "px";
+    bounds.style.width = (window.innerHeight * boundHeight).toString() + "px";
 
+    //Adjust scene to window resize
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
 
@@ -297,11 +242,10 @@ function onWindowResize() {
 
     insetWidth = window.innerHeight / 4;
     insetHeight = window.innerHeight / 4;
-
 }
 
 
-
+//Draw function
 function animate() {
     const tempV = new THREE.Vector3();
 
@@ -329,48 +273,74 @@ function animate() {
     renderer.setScissor(20, 20, insetWidth, insetHeight);
     renderer.setViewport(20, 20, insetWidth, insetHeight);
 
-    //Renderer will set this eventually
+    // Renderer will set this eventually
     matLine.resolution.set(insetWidth, insetHeight); // resolution of the inset viewport
     renderer.setScissorTest(false);
+
+    // tempV.set(spline.position);
     tempV.project(camera);
 
     // Convert the normalized position to CSS coordinates
     const x = (tempV.x * .5 + .5) * window.innerWidth;
     const y = (tempV.y * -.5 + .5) * window.innerHeight;
 
-    // Move bounds
-    bounds.style.transform = `translate(-50%, -50%) translate(${x}px,${y}px)`;
-    entryText.style.transform = `translate(-50%, -50%) translate(${x}px,${y + 145}px)`;
-
+    // Move elements with camera to center
+    // bounds.style.transform = `translate(-50%, -50%) translate(${x}px,${y}px)`;
+    // bounds.style.transform = `translate(-50%, -50%) translate(${x}px,${y}px)`;
 
 }
 
-//Move scene on mouse moves
+
+//Paralax
 window.addEventListener("mousemove", e => {
-    // scene.position.y = (e.pageY - window.innerWidth/2) * (-0.001);
-    // scene.position.x = (e.pageX - window.innerHeight/2) * (0.001);
-
     //Smooth move
-    gsap.to(scene.position, { x: (e.pageX - window.innerWidth / 2) * (-0.001), y: (e.pageY - window.innerHeight / 2) * (0.001) });
-
-    console.log(spline.getPoint(e.pageX/window.innerWidth));
-
+    if (!entered) {
+        gsap.to(scene.position, { x: (e.pageX - window.innerWidth / 2) * (0.001), y: (e.pageY - window.innerHeight / 2) * (-0.001) });
+    }
 });
+
+////////////MAIN TIMELINE/////////////////
+
+let mainTl = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".header",
+        start: "top top",
+        end: "1000",
+        pin: true,
+        scrub: 1,
+        markers: true,
+    },
+});
+
+mainTl
+    //About
+    .to(".title", 1, { color: "rgba(0,0,0,0)" }, "<")
+    .to(".title", 0.5, { fontSize: "30px", fontWeight: "400", textAlign: "left" })
+    .to(".header", 2, { alignItems: "flex-start", justifyContent: "left", paddingLeft: "30px", height: "50px" })
+    .to("#headerLogo", 2, { display: "block", opacity: 1 }, "<")
+    .to(".title", 2, { color: "#404040" }, "<")
+    .to("#aboutLink", 1, {textDecoration: "line-through"}, "<")
+
+    //Work
+    .to("#bounds", 4, { borderRadius: "0px", height: "55%",})
+    .to(spline, 4, { tension: 3.0 }, "<")
+    .to("#workLink", 1, {textDecoration: "line-through"}, "<")
+
+    //Contact
+    .to("#bounds", 4, { borderRadius: "8em", width: "16em", height: "16em", x: -200, y: -200, rotation: 45 })
+    .to(spline, 4, { tension: 0.0 }, "<")
+    .to("#contactLink", 1, {textDecoration: "line-through"}, "<")
+
+
 
 
 
 //////////////LINKS TO HTML////////////////
 
-
-
-//Entry animation
-var entryTextDelay = 8;
-gsap.to(entryText, { delay: entryTextDelay, duration: 3, color: "rgb(128,128,128)" });
-
 const isMobile = window.matchMedia("only screen and (max-width: 760px)").matches;
 
-if (isTouchDevice() && isMobile){
-    entryText.innerText = "[ tap to continue ]";
+if (isTouchDevice() && isMobile) {
+    // entryText.innerText = "[ tap to continue ]";
 }
 
 window.addEventListener("keypress", e => {
@@ -389,21 +359,31 @@ window.addEventListener("click", e => {
 
 bounds.addEventListener("mouseover", enter);
 bounds.addEventListener("mouseout", exit);
-// bounds.addEventListener("click", enterClick);
+
+document.getElementById("aboutLink").addEventListener("mouseover", function () { gsap.to(spline, { tension: 1.0 }); });
+document.getElementById("aboutLink").addEventListener("mouseout", function () { gsap.to(spline, { tension: 1.0 }); });
+document.getElementById("workLink").addEventListener("mouseover", function () { gsap.to(spline, { tension: 3.0 }); });
+document.getElementById("workLink").addEventListener("mouseout", function () { gsap.to(spline, { tension: 1.0 }); });
+document.getElementById("contactLink").addEventListener("mouseover", function () { gsap.to(spline, { tension: 0.0 }); });
+document.getElementById("contactLink").addEventListener("mouseout", function () { gsap.to(spline, { tension: 1.0 }); });
+
+
 
 function enter() {
     rotateSpeed = 0.03;
+    // gsap.to(spline, { tension: 2.0 });
 }
 
 function exit() {
     rotateSpeed = 0.01;
+    // gsap.to(spline, { tension: 1.0 });
 }
 
 function enterClick() {
     entered = true;
     setRotate = false;
-    makeSignature();
-    bounds.style.display = "none";
+    drawPage();
+    // bounds.style.display = "none";
 }
 
 function isTouchDevice() {
